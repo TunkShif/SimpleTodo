70551360e92360354a5ad2a59c05db37e4cf8c44
TunkShif <tunkshif@foxmail.com>
2021-04-28 17:54:31 +0800

    init: first commit

---
+++ src/test/kotlin/one/tunkshif/simpletodo/SimpleTodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package one.tunkshif.simpletodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleTodoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
